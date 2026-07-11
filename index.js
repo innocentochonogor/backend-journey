31a4484454949ab2a4ecf5b41fc19b2f7673add2
Ochonogor Innocent <innocentochonogor@gmail.com>
2026-07-12 00:14:42 +0100

    Practicing forEach, filter and map methods

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-const jsonString = '{"product":"Laptop","price":1200,"inStock":true}';
-const product = JSON.parse(jsonString);
+let users = [
+	{ id: 1, name: "Okonkwo", age: 59 },
+	{ id: 2, name: "Amarachi", age: 14 },
+	{ id: 3, name: "Smith", age: 36 }
+]
+console.log(users)
 
-let stockStatus = product.inStock ? "in stock" : "out of stock";
+users.forEach((user) => {
+	console.log(`User is ${user.name}. User is ${user.age} years old.`)
+});
+let adults = users.filter(user => {
+	return user.age >= 18
+});
+let names = users.map(user => {
+	return user.name
+});
 
-console.log(`The ${product.product} costs $${product.price} and is ${stockStatus}.`);
+console.log(adults)
+console.log(names)
