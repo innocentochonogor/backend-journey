67c53eb5c437c21c27c9b456e887da620cfbb818
Ochonogor Innocent <innocentochonogor@gmail.com>
2026-07-11 20:04:28 +0100

    Practicing JavaScript objects and JavaScript Object Notation - JSON, template literals

--- index.js
+++ index.js
@@ -1,23 +1,8 @@
-// Variables
-let name = "Innocent";
-const role = "Backend Developer";
-
-// Template literals (string interpolation)
-console.log(`Hello, ${name}! You're learning to be a ${role}.`);
-
-// Function
-function addNumbers(a, b) {
-  return a + b;
+const user = {
+	name: "Innocent",
+	age: 42,
+	email: "innocentochonogor@gmail.com"
 }
-
-console.log("2 + 3 =", addNumbers(2, 3));
-
-// Arrow function (modern JS style — used constantly in Node)
-const multiply = (a, b) => a * b;
-console.log("4 * 5 =", multiply(4, 5));
-
-// Arrays and loops
-const languages = ["JavaScript", "Python", "Go"];
-languages.forEach((lang) => {
-  console.log(`I could learn ${lang}`);
-});
+console.log(`My name is ${user.name}. I am ${user.age}years old. My email is ${user.email}.`);
+let jsonString = JSON.stringify(user);
+console.log(jsonString);
