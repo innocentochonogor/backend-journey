24ef4b0fe979351b6554ba75d070410f9882f775
Ochonogor Innocent <innocentochonogor@gmail.com>
2026-07-12 22:44:25 +0100

    Add route parameters with GET /users/:id

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const app = express();
+const users = [
+  { id: 1, name: "Okonkwo", age: 59 },
+  { id: 2, name: "Amarachi", age: 14 },
+  { id: 3, name: "Smith", age: 36 }
+];
+
 app.get('/', (req, res) => {
 	res.send("Welcome to the homepage!")
 });
-app.get('/about', (req, res) => {
-	res.send("This is the about page")
-});
+app.get('/users/:id', (req, res) => {
+	const id = Number(req.params.id);
+	const user = users.find(u => u.id === id);
+
+	if (user) {
+		res.json(user);
+	} else {
+               res.status(404).json({error: "User not found"});
+	}
+});	
+
+
 app.listen(3000, () => {
 	console.log("Server running on http://localhost:3000");
 });
