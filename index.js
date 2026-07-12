1e0487e5fb10b46d33bd7fcb5696d54ac5a52ef8
Ochonogor Innocent <innocentochonogor@gmail.com>
2026-07-12 23:52:09 +0100

    Add GET /users, POST /users, demonstrate in-memory data limits

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const app = express();
+app.use(express.json()); // middleware: parses incoming JSON request bodies
+
 const users = [
   { id: 1, name: "Okonkwo", age: 59 },
   { id: 2, name: "Amarachi", age: 14 },
   { id: 3, name: "Smith", age: 36 }
 ];
 
-app.get('/', (req, res) => {
-	res.send("Welcome to the homepage!")
+app.get('/users', (req, res) => {
+	res.json(users);
 });
+
 app.get('/users/:id', (req, res) => {
 	const id = Number(req.params.id);
 	const user = users.find(u => u.id === id);
@@ -20,6 +23,15 @@ app.get('/users/:id', (req, res) => {
 	}
 });	
 
+app.post('/users', (req, res) => {
+	const newUser = {
+		id: users.length + 1,
+		name: req.body.name,
+		age: req.body.age
+		};
+	users.push(newUser);
+	res.status(201).json(newUser);
+});
 
 app.listen(3000, () => {
 	console.log("Server running on http://localhost:3000");
