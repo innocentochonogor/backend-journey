1706fcb93d6e37e8a1035bac80b38938b6107094
Ochonogor Innocent <innocentochonogor@gmail.com>
2026-07-12 15:05:38 +0100

    Simulating an API response with a function

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
-let users = [
-	{ id: 1, name: "Okonkwo", age: 59 },
-	{ id: 2, name: "Amarachi", age: 14 },
-	{ id: 3, name: "Smith", age: 36 }
-]
-console.log(users)
+const users = [
+  { id: 1, name: "Okonkwo", age: 59 },
+  { id: 2, name: "Amarachi", age: 14 },
+  { id: 3, name: "Smith", age: 36 }
+];
 
-users.forEach((user) => {
-	console.log(`User is ${user.name}. User is ${user.age} years old.`)
-});
-let adults = users.filter(user => {
-	return user.age >= 18
-});
-let names = users.map(user => {
-	return user.name
-});
-
-console.log(adults)
-console.log(names)
+function getUserById(id){
+	const user = users.find(u => u.id === id)
+	if(user){
+		return user
+	} else{
+		return {error: "User not found"}
+	}
+}
+console.log(getUserById(2));
+console.log(getUserById(99));
